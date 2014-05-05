546440d5140c20687362aa91c72ba199a7b7f44c
Matthew Knudsen <mknudsen01@gmail.com>
2014-05-05 13:24:12 -0700

    Add mailing functionality
      * Uses mandrill to send emails
      * Requires dotenv to load environment vars
      * Adds Gemfile
      Next step:
      * Get it on Heroku

--- mailer.rb
+++ mailer.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'dotenv'
+require 'mandrill'
+Dotenv.load
 
 post '/' do
   p params
+  email_data = params
+  m = Mandrill::API.new ENV['MANDRILL_APIKEY']
+  message = {
+   :subject=> email_data['subject'],
+   :from_name=> "Matthew",
+   :text=> email_data['body'],
+   :to=>[
+     {
+       :email=> email_data['to'],
+       :name=> "Recipient1"
+     }
+   ],
+   :from_email=>"noreply@example.com"
+  }
+  sending = m.messages.send message
 end
